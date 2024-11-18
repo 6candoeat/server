2240ffad72d759ae8c74f2d4709a5816afa0d030
삼성이온2\sseo2 <sseo2732@gmail.com>
2024-11-18 16:24:21 +0900

    purine 계산

---
+++ src/main/java/com/_candoit/drfood/repository/IngredientRepository.java
@@ -0,0 +1,12 @@
+package com._candoit.drfood.repository;
+
+
+import com._candoit.drfood.domain.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    Ingredient findByName(String name);
+
+}
---
+++ src/main/java/com/_candoit/drfood/service/IngredientService.java
@@ -0,0 +1,14 @@
+package com._candoit.drfood.service;
+
+
+import com._candoit.drfood.menu.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IngredientService {
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+
+}
